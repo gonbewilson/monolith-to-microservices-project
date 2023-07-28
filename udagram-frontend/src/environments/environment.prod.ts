4bc32c60b9f0ccc59380301332f0fdc6e3103223
Chris <none>
2023-07-28 01:46:48 -0400

    turnin

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac98ebaf9f5484ada9d55e052fe53930-940523743.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
